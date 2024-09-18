19f4cd049f366d601ce0dfd72d84106e8ee4fa9f
ShmilyLuHongXu <86153531+ShmilyLuHongXu@users.noreply.github.com>
2024-09-18 15:06:34 +0800

    ts中的类、访问器

--- src/index.ts
+++ src/index.ts
@@ -1,101 +1,38 @@
-// 约束对象
-// interface User {
-//     name: string
-//     age: number
-//     sayHello: () => void
-// }
-// type User2 = {
-//     name: string
-//     age: number
-// }
-// let u: User = {
-//     name: 'xiaoming',
-//     age: 12,
-//     sayHello() {
-//         console.log(123)
-//     }
-// }
-//约束函数
-
-// interface Condition {
-//     (n: number): boolean
-// }
-// type Condition = (n: number) => boolean
-// type Condition = {
-//     (n: number): boolean
-// }
-// function sum(numbers: number[], callBack: Condition) {
-//     let s = 0
-//     numbers.forEach(n => {
-//         if (callBack(n)) {
-//             s += n
-//         }
-//     })
-//     return s
-// }
-// let result = sum([1, 2, 3, 4], (n) => n % 2 !== 0)
-// console.log(result)
-
-// 接口的继承
-
-// interface T1 {
-//     t1: number
-// }
-// interface T2 {
-//     t2: string
-// }
-// interface T3 extends T1, T2 {
-//     t3: boolean
-// }
-
-// type T1 = {
-//     t1: number
-// }
-// type T2 = {
-//     t2: string
-
-// }
-// type T3 = {
-//     t3: boolean
-// } & T1 & T2
-
-// let u: T3 = {
-//     t3: false,
-//     t1: 1,
-//     t2: 'e'
-// }
-
-// 只读修饰符
-// 限制对象
-interface User {
-    readonly id: string
-    name: string
-    age: number
-    readonly arrs: readonly string[]
+class User {
+    readonly name: string
+    // age: number //年龄简写
+    pid?: string
+    private _book: number
+    constructor(name: string, private _age: number, book: number, pid?: string) {
+        this.name = name
+        // this.age = age
+        if (pid !== undefined) {
+            this.pid = pid
+        }
+        this._book = book
+    }
+    // 私有参数在类内部使用
+    usebookNum() {
+        console.log(this._book)
+    }
+    // 访问器
+    set age(value: number) {
+        if (value < 0) {
+            this._age = 0
+        } else if (value > 200) {
+            this._age = 200
+        } else {
+            this._age = value
+        }
+    }
+    get age() {
+        return Math.floor(this._age)
+    }
 }
-let u: User = {
-    id: '123',
-    name: 'dfff',
-    age: 12,
-    arrs: ['q', 'w', 'e']
-}
-
-// u.id = '333'
-
-// u.arrs=['3','4']  // 只读 类似 const
-
-// u.push() //所有改变原数组的数组的方法都不可使用，因为定义的是一个只读类型的数组
-
-
-// 限制数组
-
-// let arr: readonly number[] = [1, 2, 3]
-// arr = [345, 23]
-// 上下对比，阻止数组改变用 const 定义数组
-// const arr: readonly number[] = [1, 2, 3]
-// arr = [345, 23]
-
-let arr: readonly number[] = [1, 2, 3]
-// arr.push()   //所有改变原数组的数组的方法都不可使用，因为定义的是一个只读类型的数组
-// arr[0] =2
-
+let u = new User('zhangsan', 18, 3)
+// u.name = '122'
+// console.log(u.name)
+u.usebookNum()
+u.age = 1.5
+console.log(u.age)
+console.log(u)
