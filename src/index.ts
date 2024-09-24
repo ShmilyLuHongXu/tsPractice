fe4e5e3f07129476188ab4c4d8031304ade96641
ShmilyLuHongXu <86153531+ShmilyLuHongXu@users.noreply.github.com>
2024-09-24 15:26:44 +0800

    泛型约束

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,10 @@
-// 泛型限制接口，类型别名
-// 回调函数
-// type callBack<T> = (n: T, i: number) => boolean
-// 接口
-// interface callBack<T> {
-//     (n: T, i: number): boolean
-// }
-// function filtrate<U>(arr: U[], callBack: callBack<U>): U[] {
-//     const newArr: U[] = []
-//     arr.forEach((item, i) => {
-//         if (callBack(item, i)) {
-//             newArr.push(item)
-//         }
-//     })
-//     return newArr
+interface ConstraintArray {
+    length: number
+}
 
-// }
-// let result = filtrate<number>([2, 8, 4, 5, 6, 7], n => n % 2 === 0)
-// console.log(result);
+function loggingIdentity<T extends ConstraintArray>(arg: T): T {
+    console.log(arg.length)
+    return arg
+}
 
-// 类
-import { ArrayHelper } from "./arrHelper";
-let newObj = new ArrayHelper([1, 2, 3, 4, 5])
-let arr = newObj.getNewArr()
-console.log(arr)
-console.log(newObj);
-
+loggingIdentity({ length: 3, value: '4' })
