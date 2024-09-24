3e0057955420d02fbeb8eb36a68bf107963ac0e2
ShmilyLuHongXu <86153531+ShmilyLuHongXu@users.noreply.github.com>
2024-09-24 15:40:47 +0800

    多泛型

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-interface ConstraintArray {
-    length: number
+// 将两个数组进行混合
+
+function mixinArray<T, L>(arr1: T[], arr2: L[]): (T | L)[] {
+    if (arr1.length != arr2.length) {
+        throw new Error('两个数组长度不一致')
+    }
+    let result: (T | L)[] = []
+    for (let i = 0; i < arr1.length; i++) {
+        result.push(arr1[i]);
+        result.push(arr2[i]);
+    }
+    return result
 }
 
-function loggingIdentity<T extends ConstraintArray>(arg: T): T {
-    console.log(arg.length)
-    return arg
-}
-
-loggingIdentity({ length: 3, value: '4' })
+let r = mixinArray<number, string>([1, 2, 3, 4], ['1', '3', 'true', '4'])
+console.log(r);
