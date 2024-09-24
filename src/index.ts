994ece1d6dcda7edbdd67795ee0b23dd0cd835e7
ShmilyLuHongXu <86153531+ShmilyLuHongXu@users.noreply.github.com>
2024-09-24 15:08:12 +0800

    如何在类型别名、接口、类中使用泛型

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,27 @@
+// 泛型限制接口，类型别名
+// 回调函数
+// type callBack<T> = (n: T, i: number) => boolean
+// 接口
+// interface callBack<T> {
+//     (n: T, i: number): boolean
+// }
+// function filtrate<U>(arr: U[], callBack: callBack<U>): U[] {
+//     const newArr: U[] = []
+//     arr.forEach((item, i) => {
+//         if (callBack(item, i)) {
+//             newArr.push(item)
+//         }
+//     })
+//     return newArr
 
-/**
- * 
- * @param arr 
- * @param n 
- * @returns 
- */
-function draw<T = number>(arr: T[], n: number): T[] {
-    if (n > arr.length) {
-        return arr
-    }
-    let newArr: T[] = []
-    for (let i = 0; i < n; i++) {
-        newArr.push(arr[i])
-    }
-    return newArr
-}
-let result = draw(['1', 1, '6', '4'], 3)
-console.log(result)
+// }
+// let result = filtrate<number>([2, 8, 4, 5, 6, 7], n => n % 2 === 0)
+// console.log(result);
 
+// 类
+import { ArrayHelper } from "./arrHelper";
+let newObj = new ArrayHelper([1, 2, 3, 4, 5])
+let arr = newObj.getNewArr()
+console.log(arr)
+console.log(newObj);
 
