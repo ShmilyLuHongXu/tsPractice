2f4df32b0682a2dadee76a4c459db472528ed263
ShmilyLuHongXu <86153531+ShmilyLuHongXu@users.noreply.github.com>
2024-10-10 11:01:57 +0800

    字典类创建完成

--- src/Dictionary.ts
+++ src/Dictionary.ts
@@ -1,7 +1,11 @@
+
+interface Callback<T, U> {
+    (key: T, val: U): void
+}
 export class Dictionary<k, v> {
     private keys: k[] = []
     private vals: v[] = []
-    // 判断字典中是否包含某个key，如果没有，则添加
+    // 判断字典中是否包含某个key，如果没有，则添加,如果有，则修改
     set(key: k, val: v) {
         let i = this.keys.indexOf(key)
         if (i < 0) {
@@ -10,7 +14,27 @@ export class Dictionary<k, v> {
         } else {
             this.vals[i] = val
         }
-        console.log(this.keys)
-        console.log(this.vals)
+    }
+    // 遍历字典类，接收一个回调函数
+    forEach(callback: Callback<k, v>) {
+        this.keys.forEach((k, i) => {
+            callback(k, this.vals[i])
+        })
+    }
+    // 判断字典中是否包含某个key
+    has(key: k) {
+        return this.keys.includes(key)
+    }
+    // 根据key，删除对应的键值对
+    delete(key: k) {
+        let i = this.keys.indexOf(key)
+        if (i !== -1) {
+            this.keys.splice(i, 1)
+            this.vals.splice(i, 1)
+        }
+    }
+    // 当前字段的长度
+    size() {
+        return this.keys.length
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
-// import { Dictionary } from "./Dictionary";
+import { log } from "console";
+import { Dictionary } from "./Dictionary";
 
-// const d = new Dictionary<string, number>()
-// d.set('a', 1)
-console.log(123);
+const d = new Dictionary<string, number>()
+d.set('a', 1)
+d.set('a', 11)
+d.set('b', 2)
+// d.forEach((k, v) => {
+//     console.log(k);
+//     console.log(v);
+// })
+let result = d.has('c')
+// console.log(result);
+// d.delete('a')
+// d.delete('b')
+d.forEach((k, v) => {
+    console.log(k);
+    console.log(v);
+})
+console.log(d.size());
+
